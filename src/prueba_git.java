4b7e33491975e71743e05d574caff5acf1f3b60d
juanjoseflorez <juanjoseflorezlopez@gmail.com>
2025-08-12 10:29:15 -0500

    XD ._.

--- src/prueba_git.java
+++ src/prueba_git.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
     System.out.println("buena crack (._.)");
     System.out.println();
     System.out.println("si");
+    System.out.println("Flores Papasito");
 }
 
 
